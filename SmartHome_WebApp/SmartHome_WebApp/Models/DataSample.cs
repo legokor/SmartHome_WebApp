434c2243bdae2c70c7516a58adb66df10aba43ae
Richard Schantzl <schantzl@live.com>
2017-11-27 23:52:48 +0100

    Added data model for measurment persistance

---
+++ SmartHome_WebApp/SmartHome_WebApp/Models/DataSample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartHome_WebApp.Models
+{
+    public class DataSample
+    {
+        [Key]
+        public Guid SamplingId { get; set; }
+        [Required]
+        public int SenderId { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public double CoLevel { get; set; }
+        public double SmokeLevel { get; set; }
+        public double LpgLevel { get; set; }
+
+        public int Movement { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+    }
+}

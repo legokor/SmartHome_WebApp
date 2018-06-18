75126eab56d65dd765155a020847604692c7c604
Richard Schantzl <schantzl@live.com>
2018-06-19 00:47:56 +0200

    Created new migration

--- SmartHome_WebApp/SmartHome.Persistence/Migrations/20180618204839_Concurrency.Designer.cs
+++ SmartHome_WebApp/SmartHome.Persistence/Migrations/20180618204839_Concurrency.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace SmartHome.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180618204839_Concurrency")]
+    partial class Concurrency
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SmartHome_WebApp/SmartHome.Persistence/Migrations/20180618204839_Concurrency.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SmartHome.Persistence.Migrations
+{
+    public partial class Concurrency : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MasterUnits_AspNetUsers_OwnerId",
+                table: "MasterUnits");
+
+            migrationBuilder.RenameColumn(
+                name: "OwnerId",
+                table: "MasterUnits",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_MasterUnits_OwnerId",
+                table: "MasterUnits",
+                newName: "IX_MasterUnits_UserId");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ConcurrencyLock",
+                table: "MasterUnits",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MasterUnits_AspNetUsers_UserId",
+                table: "MasterUnits",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MasterUnits_AspNetUsers_UserId",
+                table: "MasterUnits");
+
+            migrationBuilder.DropColumn(
+                name: "ConcurrencyLock",
+                table: "MasterUnits");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "MasterUnits",
+                newName: "OwnerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_MasterUnits_UserId",
+                table: "MasterUnits",
+                newName: "IX_MasterUnits_OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MasterUnits_AspNetUsers_OwnerId",
+                table: "MasterUnits",
+                column: "OwnerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
